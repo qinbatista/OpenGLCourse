c9ccb969f116e15e19ec679b53585b61a5751397
qin.batista <qin.batista@outlook.com>
2023-01-07 13:35:48 -0800

    update

--- Course2ShaderAndTriangle/InternalLibs/DisplayManager.cpp
+++ Course2ShaderAndTriangle/InternalLibs/DisplayManager.cpp
@@ -4,13 +4,12 @@
 #include <math.h>
 #include "ShaderAndTriangle.h"
 #include "glm/gtc/matrix_transform.hpp"
-#include "GLFWManager.h"
 glm::mat4 DisplayProjection;
 void Awake()
 {
     CreateObjects();
-    DisplayProjection = glm::perspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
     CreateShaders();
+    DisplayProjection = glm::perspective(45.0f, mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 100.0f);
 }
 void Update()
 {
--- Course2ShaderAndTriangle/main.cpp
+++ Course2ShaderAndTriangle/main.cpp
@@ -1,8 +1,24 @@
 #include "GL/glew.h"
-#include "InternalLibs/GLFWManager.h"
-#include "InternalLibs/GLEWManager.h"
+#include "InternalLibs/DisplayManager.h"
+#include "InternalLibs/Window.h"
+#include "glm/gtc/matrix_transform.hpp"
+Window mainWindow;
 int main()
 {
-    InitGLEW(InitGLFW(),GLFWGetWidth(), GLFWGetHeight());
+    // InitGLEW(InitGLFW(),GLFWGetWidth(), GLFWGetHeight());
+    mainWindow = Window(800, 600);
+    mainWindow.Initialise();
+    Awake();
+    while (!mainWindow.getShouldClose())
+    {
+        // Get and handle user input events
+        glfwPollEvents();
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        Update();
+        // Swap buffers
+        mainWindow.swapBuffers();
+    }
     return 0;
 }
