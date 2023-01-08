0974073a1be056382d28c82572498862a3fdd98e
qin.batista <qin.batista@outlook.com>
2023-01-08 12:51:16 -0800

    update

--- Course2ShaderAndTriangle/main.cpp
+++ Course2ShaderAndTriangle/main.cpp
@@ -12,7 +12,7 @@ int main()
     mainWindow = Window(800, 600);
     mainWindow.Initialise();
     displaySystem = DisplaySystem(mainWindow.getBufferWidth() / mainWindow.getBufferHeight());
-    camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 0.001f, 1.0f);
+    camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 1.0, 1.0f);
     displaySystem.Awake();
     while (!mainWindow.getShouldClose())
     {
@@ -21,7 +21,7 @@ int main()
         deltaTime = now - lastTime;
         lastTime = now;
         glfwPollEvents();
-        camera.keyControl(mainWindow.getKeys(), 10);
+        camera.keyControl(mainWindow.getKeys(), deltaTime);
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
