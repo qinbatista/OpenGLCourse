6ed04ee3f96d4a8a74f029bfeada497d923718ae
qin.batista <qin.batista@outlook.com>
2023-01-07 23:11:39 -0800

    update

--- Course2ShaderAndTriangle/InternalLibs/Window.h
+++ Course2ShaderAndTriangle/InternalLibs/Window.h
@@ -12,6 +12,9 @@ public:
 	GLfloat getBufferWidth() { return bufferWidth; }
 	GLfloat getBufferHeight() { return bufferHeight; }
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+    bool* getKeys() { return keys; }
+    GLfloat getXChange();
+    GLfloat getYChange();
 	void swapBuffers() { glfwSwapBuffers(mainWindow); }
 	~Window();
 
@@ -20,6 +23,12 @@ private:
 	GLfloat width, height;
 	GLint bufferWidth, bufferHeight;
     bool keys[1024];
+    GLfloat lastX;
+    GLfloat lastY;
+    GLfloat xChange;
+    GLfloat yChange;
+    bool mouseFirstMoved;
     void createCallbacks();
     static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+    static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
