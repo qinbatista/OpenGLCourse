56dc947cc2e1869b84b11777f5822cfef3a2ecf5
qin.batista <qin.batista@outlook.com>
2023-01-08 12:58:08 -0800

    update

--- Course2ShaderAndTriangle/InternalLibs/Camera.h
+++ Course2ShaderAndTriangle/InternalLibs/Camera.h
@@ -10,6 +10,7 @@ public:
     Camera();
     Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
     void keyControl(bool *keys, GLfloat deltaTime);
+    void mouseControl(GLfloat xChange, GLfloat yChange);
     glm::mat4 calculateViewMatrix();
     ~Camera();
 
